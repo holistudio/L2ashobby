a7736082c47d5751ffed873e56ea641d7a1edc02
holistudio <daniel.bin.lu@gmail.com>
2025-09-25 12:37:06 -0400

    Update random_agent.py
    Random tic-tac-toe agent
     - will avoid thinking about generalizing to any kind of action space/game for now...

---
+++ rl/q-learning/random_agent.py
@@ -0,0 +1,7 @@
+class RandomAgent(object):
+    def action(self, state):
+        # identify blank locations on the board
+
+        # randomly select among the remaining blank locations
+        
+        return loc
