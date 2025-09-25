968c890cb27082b58602a0ad9e10d97476306939
holistudio <daniel.bin.lu@gmail.com>
2025-09-25 15:57:16 -0400

    Update train.py
    Add a score board to track each player's win-loss-draws

--- rl/q-learning/train.py
+++ rl/q-learning/train.py
@@ -1,21 +1,28 @@
 from tictactoe import TicTacToe
 from random_agent import RandomAgent
 
+EPISODES = 3
+
 def main():
+    score_board = []
+
     environment = TicTacToe()
 
     agent = RandomAgent()
 
-    state, terminal, reward = environment.reset()
+    for e in range(EPISODES):
+        state, terminal, reward = environment.reset()
 
-    terminal = False
+        terminal = False
 
-    while not terminal:
-        # agent plays a piece
-        action = agent.action(state)
+        while not terminal:
+            # agent plays a piece
+            action = agent.action(state)
 
-        # move environment a step
-        state, terminal, reward = environment.step(action)
+            # move environment a step
+            state, terminal, rewards = environment.step(action)
+
+        score_board.append(rewards)
 
 
 if __name__ == "__main__":
