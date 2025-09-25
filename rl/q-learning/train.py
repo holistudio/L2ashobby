099be4244c480b12834cb08f8bb3f007b08a373a
holistudio <daniel.bin.lu@gmail.com>
2025-09-25 15:42:05 -0400

    Update train.py
    Basic game episode while loop

--- rl/q-learning/train.py
+++ rl/q-learning/train.py
@@ -1,8 +1,22 @@
 from tictactoe import TicTacToe
+from random_agent import RandomAgent
 
 def main():
     environment = TicTacToe()
-    
+
+    agent = RandomAgent()
+
+    state, terminal, reward = environment.reset()
+
+    terminal = False
+
+    while not terminal:
+        # agent plays a piece
+        action = agent.action(state)
+
+        # move environment a step
+        state, terminal, reward = environment.step(action)
+
 
 if __name__ == "__main__":
     main()
