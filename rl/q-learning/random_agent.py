fa56b9f664e0127c57f5b231746c6fd57de00ab9
holistudio <daniel.bin.lu@gmail.com>
2025-09-25 15:16:57 -0400

    Update random_agent.py
    Comments, remove debug output

--- rl/q-learning/random_agent.py
+++ rl/q-learning/random_agent.py
@@ -3,14 +3,17 @@ import random
 class RandomAgent(object):
     def action(self, state):
         def ix_to_loc(ix):
+            # convert board index into a r,c location
             r = ix // 3
             c = ix % 3
             return (r,c)
+        
         # identify blank locations on the board
         board_flat = [e for row in state for e in row]
-        print(board_flat)
+        # print(board_flat)
+        
         blank_ixs = [i for i,e in enumerate(board_flat) if e == " "]
-        print(blank_ixs)
+        # print(blank_ixs)
 
         # randomly select among the remaining blank locations
         select_ix = random.choice(blank_ixs)
