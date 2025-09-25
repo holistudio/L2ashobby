d113d4aff41474fa9103fadfba272f704c0c15b8
holistudio <daniel.bin.lu@gmail.com>
2025-09-25 16:04:53 -0400

    Update train.py
    score stats displayed at end of all episode

--- rl/q-learning/train.py
+++ rl/q-learning/train.py
@@ -3,6 +3,26 @@ from random_agent import RandomAgent
 
 EPISODES = 3
 
+def score_stats(score_board):
+    p1_stats=[0,0,0] # win-loss-draw
+    p2_stats=[0,0,0]
+    for result in score_board:
+        if result[0] == 1:
+            p1_stats[0]+=1
+            p2_stats[1]+=1
+        elif result[0] == -1:
+            p1_stats[1]+=1
+            p2_stats[0]+=1
+        else:
+            p1_stats[2]+=1
+            p2_stats[2]+=1
+
+    p1_win_rate = p1_stats[0]* 100/sum(p1_stats) 
+    p2_win_rate = p2_stats[0]* 100/sum(p2_stats)
+
+    print(f'Player 1 ({p1_win_rate:.2f}%): {p1_stats[0]} W - {p1_stats[1]} L - {p1_stats[2]} D ')
+    print(f'Player 2 ({p2_win_rate:.2f}%): {p2_stats[0]} W - {p2_stats[1]} L - {p2_stats[2]} D ')
+
 def main():
     score_board = []
 
@@ -24,6 +44,7 @@ def main():
 
         score_board.append(rewards)
 
+    score_stats(score_board)
 
 if __name__ == "__main__":
     main()
