c06ebeb2511403b9330859ceddff61334b6af6c3
holistudio <daniel.bin.lu@gmail.com>
2025-09-25 15:07:37 -0400

    Update random_agent.py
    action() selects among blank locations

--- rl/q-learning/random_agent.py
+++ rl/q-learning/random_agent.py
@@ -1,7 +1,16 @@
+import random
+
 class RandomAgent(object):
     def action(self, state):
+        def ix_to_loc(ix):
+            r = ix // 3
+            c = ix % 9
+            return (r,c)
         # identify blank locations on the board
+        board_flat = [e for row in state for e in row]
+        blank_ixs = [i for i,e in enumerate(board_flat) if e == " "]
 
         # randomly select among the remaining blank locations
-        
+        select_ix = random.choice(blank_ixs)
+        loc = ix_to_loc(select_ix)
         return loc
