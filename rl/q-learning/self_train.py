364988458b9d0db1a36bd9624acd5b7e02e37637
holistudio <daniel.bin.lu@gmail.com>
2025-10-03 22:39:10 -0400

    Update self_train.py
     - check terminal after each agent's action/environment.step()
     - if terminal, update agent experience accordingly
     - copy.deepcopy(states) is key
     - Tested with both RandomAgent and QLearningAgent
     - agent2 = agent1 so that they share same Q-table

--- rl/q-learning/self_train.py
+++ rl/q-learning/self_train.py
@@ -12,13 +12,14 @@ def main():
 
     environment = TicTacToe(display=True)
 
-    agent1 = RandomAgent()
-    # agent1 = QLearningAgent(decay_rate=0.0002)
+    # agent1 = RandomAgent()
+    agent1 = QLearningAgent(decay_rate=0.0002)
     # agent1 = QLearningAgent(alpha=1.0, gamma=1.0)
 
-    agent2 = RandomAgent()
+    # agent2 = RandomAgent()
     # agent2 = QLearningAgent(decay_rate=0.0002)
     # agent2 = QLearningAgent(alpha=1.0, gamma=1.0)
+    agent2 = agent1
 
     n_act = 0 # track total number of actions
 
@@ -29,44 +30,67 @@ def main():
         terminal = False
 
         while not terminal:
+            # print(f'state1: {state1}')
+
             # agent plays a piece
             action1 = agent1.action(state1)
-            # print(action,rewards)
             n_act += 1
 
             # move environment a step
             next_state2, terminal, rewards = environment.step(action1)
+            print(f'     state1: {state1}')
+            print(f'next_state2: {next_state2}')
+            print(f'  terminal1: {terminal}')
 
             if n_act > 1:
+                # update agent experience
+                print(f'     agent2')
+                print(f'     state2: {state2}')
+                print(f'next_state2: {next_state2}')
                 agent2.update_experience(copy.deepcopy(state2), action2, copy.deepcopy(next_state2), rewards, terminal)
             
             if not terminal:
-                state2 = next_state2
+                # update state
+                state2 = copy.deepcopy(next_state2)
+                # print(f'state2: {state2}')
 
                 # agent plays a piece
                 action2 = agent2.action(state2)
-                # print(action,rewards)
                 n_act += 1
 
                 # move environment a step
                 next_state1, terminal, rewards = environment.step(action2)
+                print(f'     state2: {state2}')
+                print(f'next_state1: {next_state1}')
+                print(f'  terminal2: {terminal}')
 
-                # update agent experience
-                agent1.update_experience(copy.deepcopy(state1), action1, copy.deepcopy(next_state1), rewards, terminal)
-
-                # update state
-                state1 = next_state1
-
-            # TODO: This may be a unneeded repeat of line 41 agent2.update_experience()
-            # else:
-            #     # if the game is over after agent1's action
-            #     # agent2 still needs to record that as experience
-            #     agent2.update_experience(copy.deepcopy(state2), action2, copy.deepcopy(next_state2), rewards, terminal)
-
-        # print(action,rewards)
+                if not terminal:
+                    # update agent experience
+                    print(f'     agent1')
+                    print(f'     state1: {state1}')
+                    print(f'next_state1: {next_state1}')
+                    agent1.update_experience(copy.deepcopy(state1), action1, copy.deepcopy(next_state1), rewards, terminal)
+                    # update state
+                    state1 = copy.deepcopy(next_state1)
+                else:
+                    # if the game is over after agent2's action
+                    # agent2 still needs to record that as experience
+                    print(f'     agent2')
+                    print(f'     state2: {state2}')
+                    print(f'next_state2: {next_state1}')
+                    agent2.update_experience(copy.deepcopy(state2), action2, copy.deepcopy(next_state1), rewards, terminal)
+            else:
+                # if the game is over after agent1's action
+                # agent1 still needs to record that as experience
+                print(f'     agent1')
+                print(f'     state1: {state1}')
+                print(f'next_state2: {next_state2}')
+                agent1.update_experience(copy.deepcopy(state1), action1, copy.deepcopy(next_state2), rewards, terminal)
 
         # update agent policy
+        print('Player-X')
         agent1.update_policy()
+        print('\nPlayer-O')
         agent2.update_policy()
 
     environment.score_stats(display=True)
