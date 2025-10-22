ce657549d9f4cec694537932e023a525558fd05b
holistudio <daniel.bin.lu@gmail.com>
2025-10-22 13:58:22 -0400

    Update exercise1_2.py
    Updated gymnasium environment

--- rl/spinningup/spinup/exercises/pytorch/problem_set_1/exercise1_2.py
+++ rl/spinningup/spinup/exercises/pytorch/problem_set_1/exercise1_2.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
 
     ActorCritic = partial(exercise1_2_auxiliary.ExerciseActorCritic, actor=MLPGaussianActor)
     
-    ppo(env_fn = lambda : gym.make('InvertedPendulum-v2'),
+    ppo(env_fn = lambda : gym.make('InvertedPendulum-v5'),
         actor_critic=ActorCritic,
         ac_kwargs=dict(hidden_sizes=(64,)),
         steps_per_epoch=4000, epochs=20, logger_kwargs=dict(output_dir=logdir))
