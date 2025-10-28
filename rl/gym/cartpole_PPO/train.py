b03b7fceb487f79296bea6f8a26c63cceef31010
holistudio <daniel.bin.lu@gmail.com>
2025-10-28 14:32:47 -0400

    Update train.py
    - only env.reset() returns a tuple for observation now (gymnasium update)
    - turn off terminal output
    - debug to get it running at least

--- rl/gym/cartpole_PPO/train.py
+++ rl/gym/cartpole_PPO/train.py
@@ -1,26 +1,30 @@
 import numpy as np
 import torch
-from torch.optim import Adam
+
 import gymnasium as gym
-import tqdm
+from tqdm import tqdm
 
 from agent import PPOAgent
 
-from utils.mpi_tools import num_procs
+from utils.mpi_tools import proc_id, num_procs
 
-def train(n_episodes=50, steps_per_ep=4000, max_ep_len=1000):
-    env = gym.make("CartPole-v1")
-    env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
+def train(n_episodes=10, steps_per_ep=4000, max_ep_len=1000, seed=0, ac_kwargs=dict()):
+    # Random seed
+    seed += 10000 * proc_id()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
 
-    obs_dim = env.observation_space.shape
-    act_dim = env.action_space.shape
+    env = gym.make("CartPole-v1", max_episode_steps=max_ep_len)
 
-    agent = PPOAgent(env.observation_space, env.action_space)
+    agent = PPOAgent(env.observation_space, env.action_space, **ac_kwargs)
 
     # Set up experience buffer
     local_steps_per_ep = int(steps_per_ep / num_procs())
     buf = agent.buffer
 
+    # Prepare for interaction with environment
+    (o, _), ep_ret, ep_len = env.reset(), 0, 0
+    
     # Main loop: collect experience in env and update/log each episode
     for episode in tqdm(range(n_episodes)):
         for t in range(local_steps_per_ep):
@@ -35,32 +39,34 @@ def train(n_episodes=50, steps_per_ep=4000, max_ep_len=1000):
             
             # Update obs (critical!)
             o = next_o
-            if type(o) == tuple:
-                o = o[0]
 
             timeout = ep_len == max_ep_len
             terminal = d or timeout
             ep_ended = t==local_steps_per_ep-1
 
             if terminal or ep_ended:
-                if ep_ended and not(terminal):
-                    print('Warning: trajectory cut off by episode at %d steps.'%ep_len, flush=True)
+                # if ep_ended and not(terminal):
+                #     print('Warning: trajectory cut off by episode at %d steps.'%ep_len, flush=True)
+
                 # if trajectory didn't reach terminal state, bootstrap value target
                 if timeout or ep_ended:
                     _, v, _ = agent.step(torch.as_tensor(o, dtype=torch.float32))
                 else:
                     v = 0
+
                 buf.finish_path(v)
-                if terminal:
-                    print(f'episode Reward={ep_ret}, episode Length={ep_len}')
-                o, ep_ret, ep_len = env.reset(), 0, 0
-                if type(o) == tuple:
-                    o = o[0]
+
+                # if terminal:
+                #     print(f'episode Reward={ep_ret}, episode Length={ep_len}')
+
+                (o, _), ep_ret, ep_len = env.reset(), 0, 0
         
         # PPO agent update
         agent.update()
-
-        # TODO: save agent 
+    
+    env.close()
+    
+    # TODO: save agent 
 
 if __name__ == '__main__':
     train()
